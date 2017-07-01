73c070506e9110285345b90e167bee4acbca0903
Nacho Cabanes <nacho@nachocabanes.com>
2017-07-01 11:50:36 +0200

    Display primes up to a certain number

---
+++ FreePascal/math/Primes1.pas
@@ -0,0 +1,89 @@
+(*
+  Category: SWAG Title: MATH ROUTINES
+  Original name: 0017.PAS
+  Description: PRIMES1.PAS
+  Author: SWAG SUPPORT TEAM
+  Date: 05-28-93  13:50
+*)
+
+{
+SAM HASINOFF
+
+LoopNum forget who first asked this question, but here is some code to help
+you find your prime numbers in its entirety (tested):
+
+Note: minor correction by Nacho, 2017: 1 is not a prime 
+
+}
+
+Uses
+  Crt;
+
+Label
+  get_out;
+Var
+  LoopNum,
+  Count,
+  MinPrime,
+  MaxPrime,
+  PrimeCount : Integer;
+  Prime      : Boolean;
+  max        : String[20];
+  ECode      : Integer;
+begin
+  minprime := 0;
+  maxprime := 0;
+
+  ClrScr;
+  Write('Lower limit For prime number search [1]: ');
+  readln(max);
+  val(max, minprime, ECode);
+
+  if (minprime < 2) then
+    minprime := 2;
+  Repeat
+    GotoXY(1, 2);
+    ClrEol;
+    Write('Upper limit: ');
+    readln(max);
+    val(max, maxprime, ECode);
+  Until (maxprime > minprime);
+
+  WriteLn;
+  PrimeCount := 0;
+
+  For LoopNum := minprime to maxprime do
+  begin
+    prime := True;
+    Count := 2;
+
+    While (Count <= (LoopNum div 2)) and (Prime) do
+    begin
+      if (LoopNum mod Count = 0) then
+        prime := False;
+      Inc(Count);
+    end;
+
+    if (Prime) then
+    begin
+      Write('[');
+      TextColor(white);
+      Write(LoopNum);
+      TextColor(lightgray);
+      Write('] ');
+      Inc(PrimeCount);
+      if WhereX > 75 then
+        Write(#13#10);
+    end;
+    if WhereY = 24 then
+    begin
+      Write('-- More --');
+      ReadKey;
+      ClrScr;
+    end;
+    prime := False;
+  end;
+  Write(#13#10#10);
+  Writeln(PrimeCount, ' primes found.', #13#10);
+end.
+
