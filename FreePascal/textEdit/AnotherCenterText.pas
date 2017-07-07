24c6dbcca3892857b81ef77aad6efca799c8a3c8
Nacho Cabanes <nacho@nachocabanes.com>
2017-07-07 16:07:04 +0200

    Center a text on screen

---
+++ FreePascal/textEdit/AnotherCenterText.pas
@@ -0,0 +1,22 @@
+(*
+  Category: SWAG Title: TEXT EDITING ROUTINES
+  Original name: 0002.PAS
+  Description: Another Center Text
+  Author: SWAG SUPPORT TEAM
+  Date: 05-28-93  14:08
+*)
+
+{ Center Text }
+
+Uses Crt;
+Var
+  s : String;
+  i : Integer;
+begin
+  Write('String? ');
+  readln(s);
+  i := (succ(lo(windmax)) - length(s)) shr 1;
+  gotoXY(i,10);
+  Write(s);
+end.
+
