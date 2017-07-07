e30cdab287e94f6095cb68a9895aa34340a25ca9
Nacho Cabanes <nacho@nachocabanes.com>
2017-07-07 16:08:10 +0200

    Shell sort

---
+++ FreePascal/sorting/ShellSorting.pas
@@ -0,0 +1,47 @@
+(*
+  Category: SWAG Title: SORTING ROUTINES
+  Original name: 0036.PAS
+  Description: Shell Sorting
+  Author: MATT HARGETT
+  Date: 08-27-93  21:58
+*)
+
+{
+MATT HARGETT
+
+: want to use the normal ole' bubble sorts and the like (on the order of N),
+: for the mere fact that it's just plain old slow!  Could anyone please post
+: some code, or pseudo-code of a sort that is on the order of NxLog N?  It wo
+}
+
+Program ShellSort;
+
+Var
+  A      : Array [1..1000] of Word;
+  I, J, N,
+  K, Tmp : Integer;
+
+Begin
+  N := 1000;
+  For I := 1 to N Do
+  Begin
+    A[I] := Random(5000) + 1;
+    Write(A[I] : 6);
+  End;
+
+  For K := 3 DownTo 1 Do
+    For I := 1 to N - 1 Do
+      For J := I + 1 to N Do
+        If A[J] < A[I]
+          then
+          Begin
+            Tmp  := A[J];
+            A[J] := A[I];
+            A[I] := Tmp;
+          End;
+
+  Writeln;
+
+  For I := 1 To N Do
+    Write(A[I] : 6);
+End.
