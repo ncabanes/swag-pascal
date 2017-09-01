5b50a01583db5ba4b721174a68771b96e534dcdb
Nacho Cabanes <nacho@nachocabanes.com>
2017-09-01 17:53:38 +0200

    Checking if a directory exists

---
+++ FreePascal/dirs/CheckForDirectory.pas
@@ -0,0 +1,37 @@
+(*
+  Category: SWAG Title: DIRECTORY HANDLING ROUTINES
+  Original name: 0018.PAS
+  Description: Check for Directory
+  Author: MARTIN RICHARDSON
+  Date: 09-26-93  09:10
+*)
+
+uses dos;
+
+{*****************************************************************************
+ * Function ...... IsDir()
+ * Purpose ....... To check for the existance of a directory
+ * Parameters .... Dir        Dir to check for
+ * Returns ....... TRUE if Dir exists
+ * Notes ......... None
+ * Author ........ Martin Richardson
+ * Date .......... May 13, 1992
+ *****************************************************************************}
+FUNCTION IsDir( Dir: STRING ) : BOOLEAN;
+VAR
+   fHandle: FILE;
+   wAttr: WORD;
+BEGIN
+     WHILE Dir[LENGTH(Dir)] = '\' DO DEC( Dir[0] ); { Remove trailing "\" }
+     Dir := Dir + '\.';
+     ASSIGN( fHandle, Dir );
+     GETFATTR( fHandle, wAttr );
+     IsDir := ( (wAttr AND DIRECTORY) = DIRECTORY );
+END;
+
+BEGIN
+    if IsDir('..\dirs') then
+        WriteLn('"dirs" found')
+    else
+        WriteLn('"dirs" not found');
+END.
