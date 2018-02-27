fecf6e580edd1cbf26821ffe40292312d6edf5d4
Nacho Cabanes <nacho@nachocabanes.com>
2018-02-27 22:34:23 +0100

    Changing prompt in a shell

---
+++ FreePascal/exec/EasyPromptChangingInShells.pas
@@ -0,0 +1,30 @@
+(*
+  Category: SWAG Title: EXECUTION ROUTINES
+  Original name: 0027.PAS
+  Description: Easy Prompt Changing in Shells
+  Author: STEVE ROGERS
+  Date: 11-26-94  05:02
+*)
+
+{
+  That won't work, actually. He wants to change the _prompt_ in his
+  shell. Here's one way:
+}
+
+uses
+  dos;
+
+var
+  f : text;
+
+begin
+  assign(f,'chgprmpt.bat');
+  rewrite(f);
+  writeln(f,'@set prompt=Type EXIT to return to '+paramstr(0)+'$g');
+  close(f);
+
+  swapvectors;
+  exec(getenv('COMSPEC'),'/K chgprmpt.bat');
+  swapvectors;
+end.
+
