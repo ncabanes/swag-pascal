bd5ac5075e771b8e188827d5629355ec1a591fc2
Nacho Cabanes <nacho@nachocabanes.com>
2018-02-27 22:32:09 +0100

    Compile all the units in a dir

---
+++ FreePascal/exec/CompileAllUnitsInDir.pas
@@ -0,0 +1,37 @@
+(*
+  Category: SWAG Title: EXECUTION ROUTINES
+  Original name: 0046.PAS
+  Description: Compile all units in DIR
+  Author: SUNE MARCHER
+  Date: 05-31-96  09:17
+*)
+
+uses dos;
+
+var
+  info:searchrec;
+  paspth:string;
+  de{,de2}:word;
+
+begin
+  writeln('Compiling all pascal files.');
+  paspth:=fsearch('BPC.EXE',getenv('PATH'));
+  if(paspth='')then
+  begin
+    writeln('Couldn''t find pascal compiler.');
+    halt(1);
+  end;
+  findfirst('*.pas',$ffff,info);
+  if(info.name='')then
+  begin
+    writeln('No .PAS files found.');
+    halt(1);
+  end;
+  repeat
+    {de2:=}exec(paspth,' '+info.name+' /q /build -$g+ -$r- -$d-');
+    write('Compiled ',info.name,'...');
+    {if(de2=0)then writeln('√')else writeln('%');}
+    findnext(info);
+    de:=doserror;
+  until(de<>0);
+end.
